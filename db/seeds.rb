f4a037a8118fb3a45e32b5f4dbcd95cbbabad5c5
Jason Doggart <jasondoggart@gmail.com>
2018-02-02 21:11:00 -0500

    begin working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Jason", last_name: "Doggart", email: "jdoggart@zeton.com", password: "password")
+User.create(first_name: "Leisl", last_name: "Dukhedin-Lalla", email: "ldukhedin@zeton.com", password: "password")
+User.create(first_name: "John", last_name: "Onesi", email: "jonesi@zeton.com", password: "password")
+User.create(first_name: "Oliver", last_name: "Orrell", email: "oorrell@zeton.com", password: "password")
+User.create(first_name: "Ian", last_name: "Jessop", email: "ijessop@zeton.com", password: "password")
+User.create(first_name: "Matt", last_name: "Twynstra", email: "mtwynstra@zeton.com", password: "password")
+User.create(first_name: "Danielle", last_name: "DeSousa", email: "ddesousa@zeton.com", password: "password")
+User.create(first_name: "Pallavi", last_name: "Dziewa", email: "pdziewa@zeton.com", password: "password")
+User.create(first_name: "Ed", last_name: "De Rubeis", email: "ederubeis@zeton.com", password: "password")
+User.create(first_name: "John", last_name: "Grieve", email: "jgrieve@zeton.com", password: "password")
+User.create(first_name: "Mark", last_name: "Pulcine", email: "mpulcine@zeton.com", password: "password")
+User.create(first_name: "William", last_name: "Beckley", email: "wbeckley@zeton.com", password: "password")
+User.create(first_name: "Nigel", last_name: "Semegen", email: "nsemegen@zeton.com", password: "password")
+User.create(first_name: "Paul", last_name: "Martin", email: "pmartin@zeton.com", password: "password")
+User.create(first_name: "John", last_name: "Grieve", email: "jgrieve@zeton.com", password: "password")
+
+
